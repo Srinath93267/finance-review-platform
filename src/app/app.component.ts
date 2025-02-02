4c505a37a269fe069c627f36c17c8a59670906a4
Srinath579 <62790179+Srinath579@users.noreply.github.com>
2025-02-02 20:18:51 +0530

    Added Finance Dashboard component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { FinanceDrawerComponent } from "./finance-drawer/finance-drawer.component";
+import { FinanceFooterComponent } from "./finance-footer/finance-footer.component";
+import { CommonModule, isPlatformBrowser } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FinanceDrawerComponent, FinanceFooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'finance-review-platform';
+  isLoading = false;
+  
+  constructor(public router: Router, @Inject(PLATFORM_ID) private platformId: Object) { }
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    if (isPlatformBrowser(this.platformId)) initFlowbite();
+    this.isLoading = false;
+  }
+  
 }
