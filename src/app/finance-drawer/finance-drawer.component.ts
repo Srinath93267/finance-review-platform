a7ac1aa96268a92ba4b83bcbfa74806e122ff873
Srinath579 <62790179+Srinath579@users.noreply.github.com>
2025-02-03 21:04:02 +0530

    Added finance drawer component

---
+++ src/app/finance-drawer/finance-drawer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-finance-drawer',
+  standalone: true,
+  imports: [],
+  templateUrl: './finance-drawer.component.html',
+  styleUrl: './finance-drawer.component.css'
+})
+export class FinanceDrawerComponent {
+
+}
