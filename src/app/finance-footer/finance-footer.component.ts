6943da715cd65b007a765fafae5ef3f8b4a85873
Srinath579 <62790179+Srinath579@users.noreply.github.com>
2025-02-06 20:59:32 +0530

    Added Footer Template

---
+++ src/app/finance-footer/finance-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-finance-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './finance-footer.component.html',
+  styleUrl: './finance-footer.component.css'
+})
+export class FinanceFooterComponent {
+
+}
