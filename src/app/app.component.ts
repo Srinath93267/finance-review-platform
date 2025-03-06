885a1c06c64e1737bffe959a1740e12e4326d406
Srinath579 <62790179+Srinath579@users.noreply.github.com>
2025-03-06 11:11:19 +0530

    - Changed the finance drawer component from drawer to navbar. - Created the app.service.ts for API calls. - Created environment.ts.example to dynamically create the enviornment.ts file which stores the environment variables. - Included the actual environment.ts to the .gitignore file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { initFlowbite } from 'flowbite';
 import { FinanceDrawerComponent } from "./finance-drawer/finance-drawer.component";
 import { FinanceFooterComponent } from "./finance-footer/finance-footer.component";
 import { CommonModule, isPlatformBrowser } from "@angular/common";
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FinanceDrawerComponent, FinanceFooterComponent, CommonModule],
+  imports: [RouterOutlet, FinanceDrawerComponent, FinanceFooterComponent, CommonModule, FormsModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
