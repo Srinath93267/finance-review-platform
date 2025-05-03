f30854e4fcc6afa1c6a745acac2c5a524585d79f
Srinath579 <62790179+Srinath579@users.noreply.github.com>
2025-05-03 21:18:29 +0530

    - Changed the Spinner size on Initial page loading. - Footer template changes.

--- src/app/finance-footer/finance-footer.component.ts
+++ src/app/finance-footer/finance-footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-finance-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './finance-footer.component.html',
   styleUrl: './finance-footer.component.css'
 })
