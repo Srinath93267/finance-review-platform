4790b85005a50ed18cb16b369fbf080cb65da482
Srinath579 <62790179+Srinath579@users.noreply.github.com>
2025-05-08 09:18:39 +0530

    - removed unwanted

--- src/app/generate-report/generate-report.component.ts
+++ src/app/generate-report/generate-report.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectReportsComponent } from "../select-reports/select-reports.component";
-import { TitlePageComponent } from "../title-page/title-page.component";
 import { ReviewSubmitComponent } from "../review-submit/review-submit.component";
 import { AppService, Reportflow } from '../app.service';
 
